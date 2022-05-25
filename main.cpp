9f8f50e06ac0568cfce9f0b27078ab072458a6aa
Aaron Bantay <aaronbantay@gmail.com>
2022-05-25 13:05:05 -0700

    Changed code so bulk of the function is working with the s string reference copy. Added comments

--- main.cpp
+++ main.cpp
@@ -22,22 +22,29 @@ int main() {
 void flipString(string &s) {
    // make a copy of the string reference before using substr
    // can't pass substr as a parameter when the param is a reference to a string object
-   string tempStr = "none";
+   string tempStr = s;
    // stores the front and back of the strings before removing them via substr()
    char fTemp = ' ';
    char bTemp = ' ';
 
-   // base case
-   if (s.size() <= 1) {
-      s = s;
-   } else {
-      fTemp = s.front();
-      bTemp = s.back();
-      tempStr = s.substr(1, s.size() - 2);
-      flipString(tempStr);
+   // base case is: if the parameter string's size is less than or equal to 0, then stop
+   // changed the if to do the recursive call if the string's size is GREATER than 1
+   // otherwise, we would have a conditional that does nothing
+   if (s.size() > 1) {
+      // work with the copy of the string reference parameter
+      fTemp = tempStr[0];
+      bTemp = tempStr[tempStr.size() - 1];
+
+      // remove the first and last letters in the string
+      tempStr = tempStr.substr(1, tempStr.size() - 2);
+      flipString(tempStr); // recursive call
+
+      // concatenate the string after the recursive call(s) are complete. Save it to the string reference
+      // this essentially swaps the string as well
       s = bTemp + tempStr + fTemp;
-      cout << s << endl;
-   }
+
+      //cout << s << endl;
+   } 
 }
 
 
